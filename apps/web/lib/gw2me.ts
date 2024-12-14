3fb388e29e3330b469aa198c3d926a9272b828a0
darthmaim <git@darthmaim.de>
2024-12-14 19:03:56 +0100

    Mark access to env variables as dynamic

--- apps/web/lib/gw2me.ts
+++ apps/web/lib/gw2me.ts
@@ -1,8 +1,17 @@
 import { Gw2MeClient } from '@gw2me/client';
+import { unstable_noStore } from 'next/cache';
 
-export const gw2me = new Gw2MeClient({
-  client_id: process.env.GW2ME_CLIENT_ID!,
-  client_secret: process.env.GW2ME_CLIENT_SECRET!,
-});
+let gw2me: Gw2MeClient;
+export function getGw2MeClient() {
+  unstable_noStore();
+  return gw2me ?? (gw2me = new Gw2MeClient({
+    client_id: process.env.GW2ME_CLIENT_ID!,
+    client_secret: process.env.GW2ME_CLIENT_SECRET!,
+  }));
+}
 
-export const gw2meRedirectUrl = new URL('/login/callback', process.env.BASE_URL!).toString();
+let url: string;
+export function getGw2meRedirectUrl() {
+  unstable_noStore();
+  return url ?? (url = new URL('/login/callback', process.env.BASE_URL!).toString());
+}
