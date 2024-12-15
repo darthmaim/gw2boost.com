324d89d1ea4b8a62e8a05d0d3baef34f56c7e3f1
darthmaim <git@darthmaim.de>
2024-12-15 13:19:08 +0100

    Link username to dashboard

--- apps/web/components/UserButton.tsx
+++ apps/web/components/UserButton.tsx
@@ -2,13 +2,14 @@ import { getUser, logoutAction } from '@/lib/session';
 import type { FC } from 'react';
 import styles from './UserButton.module.css';
 import { Button } from './Button';
+import Link from 'next/link';
 
 export const UserButton: FC = async () => {
   const user = await getUser();
 
   return user ? (
     <div className={styles.user}>
-      <span>{user.name}</span>
+      <Link href="/dashboard">{user.name}</Link>
       <form action={logoutAction}><Button type="submit" appearance="secondary">Logout</Button></form>
     </div>
   ) : (
