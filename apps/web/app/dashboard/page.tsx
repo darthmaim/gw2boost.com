ed90e2e9d2a34623b84a9959f12b8d567317345d
darthmaim <git@darthmaim.de>
2024-12-15 13:31:44 +0100

    List projects on dashboard

--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -1,9 +1,27 @@
+import { Button } from "@/components/Button";
+import { db } from "@/lib/db";
 import { requireUser } from "@/lib/session";
 
 export default async function DashboardRoute() {
   const user = await requireUser();
+  const projects = await db.project.findMany({
+    where: { members: { some: { userId: user.id }}}
+  });
 
   return (
-    <div>Hello {user.name}.</div>
+    <div>
+      <div>Hello {user.name}.</div>
+
+      <h1>Projects</h1>
+
+      <ul>
+        {projects.map((project) => (
+          <li key={project.id}>{project.name}</li>
+        ))}
+      </ul>
+
+      <Button type="link" href="/projects/create">Create project</Button>
+    </div>
   );
 }
+
