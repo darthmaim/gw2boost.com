99dba6ae5783cda5e7ce475376703aa26186c4cc
darthmaim <git@darthmaim.de>
2024-12-15 13:17:49 +0100

    Remove login button from landingpage

--- apps/web/app/(landingpage)/page.tsx
+++ apps/web/app/(landingpage)/page.tsx
@@ -1,13 +1,10 @@
 import styles from "./page.module.css";
-import { Button } from "@/components/Button";
 
 export default function Home() {
   return (
     <div className={styles.placeholder}>
       <h1>GW2Boost</h1>
-      Coming soon...<br/>
-
-      <Button type="link" href="/login">Login</Button>
+      Coming soon...
     </div>
   );
 }
